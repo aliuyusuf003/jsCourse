8ef99d44e7a2dde8eb9fe0c42d1d49bfcb978196
Aliu Yusuf <yusuf.aliu@taviatech.com.ng>
2020-01-06 12:20:15 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+a = 3;
+b = 7;
+num  =a+b;
+var template = `The result of adding the two numbers is <em>${num}</em> `;
+document.getElementById('test').innerHTML = template;
+// console.log(template)
